80e529b27f162dae1b3b039a776b5fcba17dbfcf
Moi <moi@MacBook-Pro-de-Moi.local>
2019-10-18 20:00:36 +0200

    handler_si

--- srcs/handle_signals.c
+++ srcs/handle_signals.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <sh21.h>
+#include <input.h>
 
 /*
 ** Handle signal when inside a fork, during execution
@@ -18,7 +19,16 @@
 
 void	handle_signals_proc(int sig)
 {
-	printf("Je suis appele\n");
+	//printf("Je suis appele\n");
 	if (sig == SIGINT)
+	{
+		ioctl(0, TIOCSTI, "\7");
+		ft_putchar('\b');
+		ft_putchar(127);
+		ft_putchar('\b');
+		ft_putchar('\b');
+		ft_putchar(127);
+		ft_putchar('\b');
 		ft_putstr("\n");
+	}
 }
