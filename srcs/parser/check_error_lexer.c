6912dfa8ee3deb4c2440945639ee92eaca6376b5
Yaelle MARCILLY <ymarcill@e1r4p9.42.fr>
2019-10-21 14:36:19 +0200

    check error

--- srcs/parser/check_error_lexer.c
+++ srcs/parser/check_error_lexer.c
@@ -1,14 +1,14 @@
 #include <input.h>
 
-int		is_elem_error(t_coord c, char **tab_q)
+int	is_elem_error(t_coord c, char **tab_q)
 {
 	c.c  = tab_q[c.i][c.j];
 	c.k = tab_q[c.i][c.j + 1];
 	c.x = c.k && tab_q[c.i][c.j + 2] ? tab_q[c.i][c.j+ 2] : 0;;
 	if ((c.j > 0 && tab_q[c.i][c.j - 1] == '&') || (c.c == '<' && c.k &&
-	(c.k == '&' || c.k == '>'|| (c.k == c.c && c.x))) || (c.c == '>' &&
-	tab_q[c.i][c.j + 1] && ((c.k == '&' && (!ft_isdigit(c.x) && c.x != '-'))
-	|| tab_q[c.i][c.j + 1] == '<' || (tab_q[c.i][c.j + 1] == c.c && c.x))))
+		(c.k == '&' || c.k == '>'|| (c.k == c.c && c.x))) || (c.c == '>' &&
+		tab_q[c.i][c.j + 1] && ((c.k == '&' && (!ft_isdigit(c.x) && c.x != '-'))
+		|| tab_q[c.i][c.j + 1] == '<' || (tab_q[c.i][c.j + 1] == c.c && c.x))))
 	{
 		ft_putstr_fd("Ymarsh: parse error near: \'", 2);
 		tab_q[c.i + 1] ? ft_putstr_fd(tab_q[c.i + 1], 2) : ft_putstr_fd(tab_q[c.i], 2);
@@ -18,7 +18,7 @@ int		is_elem_error(t_coord c, char **tab_q)
 	return (0);
 }
 
-int		is_next_elem_error(t_coord c, char **tab_q)
+int	is_next_elem_error(t_coord c, char **tab_q)
 {
 	if (ft_strcmp(tab_q[c.i], "<<") == 0 || ft_strcmp(tab_q[c.i], ">>") == 0
 		|| ft_strcmp(tab_q[c.i], ">&") == 0 || ft_strcmp(tab_q[c.i], ">") == 0
@@ -28,7 +28,8 @@ int		is_next_elem_error(t_coord c, char **tab_q)
 			|| tab_q[c.i + 1][c.j] == '>')) ||  !tab_q[c.i + 1])
 		{
 			ft_putstr_fd("ymarsh: parse error near: \'", 2);
-			tab_q[c.i + 1] ? ft_putstr_fd(tab_q[c.i + 1], 2) : ft_putstr_fd(tab_q[c.i], 2);
+			tab_q[c.i + 1] ? ft_putstr_fd(tab_q[c.i + 1], 2) :
+							ft_putstr_fd(tab_q[c.i], 2);
 			ft_putendl_fd("\'", 2);
 			return (-1);
 		}
@@ -36,9 +37,10 @@ int		is_next_elem_error(t_coord c, char **tab_q)
 	return (0);
 }
 
-int		is_file_error(t_coord c, char **tab_q)
+int	is_file_error(t_coord c, char **tab_q)
 {
-	if (ft_strcmp(tab_q[c.i], "<") == 0 && tab_q[c.i + 1] &&  access(tab_q[c.i + 1], F_OK) == -1)
+	if (ft_strcmp(tab_q[c.i], "<") == 0 && tab_q[c.i + 1]
+		&& access(tab_q[c.i + 1], F_OK) == -1)
 	{
 		ft_putstr_fd("ymarsh: no such file or directory: ", 2);
 		ft_putendl_fd(tab_q[c.i + 1], 2);
@@ -47,7 +49,7 @@ int		is_file_error(t_coord c, char **tab_q)
 	return (0);
 }
 
-int		check_error_lexer(char **tab_q)
+int	check_error_lexer(char **tab_q)
 {
 	t_coord	c;
 
@@ -60,7 +62,8 @@ int		check_error_lexer(char **tab_q)
 		{
 			if (tab_q[c.i][c.j] == '\"' || tab_q[c.i][c.j] == '\'')
 				c.q_state = c.q_state == 0 ? 1 : 0;
-			if (c.q_state == 0 && (tab_q[c.i][c.j] == '>' || tab_q[c.i][c.j] == '<'))
+			if (c.q_state == 0 && (tab_q[c.i][c.j] == '>' ||
+				tab_q[c.i][c.j] == '<'))
 			{
 				if (is_elem_error(c, tab_q) == -1)
 					return (-1);
