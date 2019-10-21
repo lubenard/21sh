7c1fb8a8a15ee4bc6fb878bc14baa19ddd844345
Yaelle MARCILLY <ymarcill@e1r4p9.42.fr>
2019-10-21 15:29:32 +0200

    ceci est encore un msg

--- srcs/redirections/heredoc.c
+++ srcs/redirections/heredoc.c
@@ -6,32 +6,11 @@
 /*   By: ymarcill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 10:59:34 by ymarcill          #+#    #+#             */
-/*   Updated: 2019/10/21 14:22:20 by ymarcill         ###   ########.fr       */
+/*   Updated: 2019/10/21 15:03:27 by ymarcill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <input.h>
-
-void	free_fornorme(int i, t_coord *c, char **arg_tab)
-{
-	char	*tmp2;
-
-	if (i == 0)
-	{
-		free(c->buf);
-		free(c->coord);
-	}
-	else if (i == 1)
-	{
-		if (arg_tab[1])
-			tmp2 = ft_strdup(g_mainline);
-		else
-			tmp2 = ft_strjoin(c->tmp, g_mainline);
-		free(c->tmp);
-		c->tmp = ft_strjoin(tmp2, "\n");
-		free(tmp2);
-	}
-}
 
 char	**fill_arg_tab(char **tab_line, char **arg_tab, int *j)
 {
